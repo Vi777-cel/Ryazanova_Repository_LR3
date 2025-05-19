c7a5ed61847a9d84998d5cc0f54034150879f2f9
Vika <Viksis1234@mail.ru>
2025-05-19 17:30:26 +0300

    create void main

---
+++ Ryazanova_LR3_project.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <limits> // Для std::numeric_limits
+using namespace std;
+
+void inputBytes(unsigned long long& B) {
+}
+
+void toMegabytes(unsigned long long B) {
+}
+
+void toGigabytes(unsigned long long B) {
+    if (B == 0) {
+        cout << "Сначала введите объем данных (пункт 1).\n";
+        return;
+    }
+    double GB = static_cast<double>(B) / (1024 * 1024 * 1024);
+    cout << B << " байт = " << GB << " гигабайт (GB)\n";
+}
+
+int main() {
+    unsigned long long B = 0;
+    int choice;
+    
+    do {
+        cout << "\nМеню:\n";
+        cout << "1. Ввести объем информации в байтах (B)\n";
+        cout << "2. Выразить объем B в мегабайтах\n";
+        cout << "3. Выразить объем B в гигабайтах\n";
+        cout << "0. Выход\n";
+        cout << "Выберите пункт меню: ";
+        
+        while (!(cin >> choice)) {
+            cout << "Ошибка ввода! Введите число: ";
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        }
+        
+        switch(choice) {
+            case 1:
+                inputBytes(B);
+                break;
+            case 2:
+                toMegabytes(B);
+                break;
+            case 3:
+                toGigabytes(B);
+                break;
+            case 0:
+                cout << "Выход из программы.\n";
+                break;
+            default:
+                cout << "Неверный выбор. Попробуйте снова.\n";
+        }
+    } while(choice != 0);
+    
+    return 0;
+}
